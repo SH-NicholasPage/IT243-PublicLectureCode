568bf3ef1f7a6e9bd06676f3feebc887aa7891ee
Nicholas Page <npage@southhills.edu>
2024-12-07 15:48:23 -0500

    Better colors + past event

--- week2/countdown/src/App.vue
+++ week2/countdown/src/App.vue
@@ -6,7 +6,7 @@
 import Event from "@/components/Event.vue";
 
 const eventData = [
-  {
+{
     id: 1,
     name: "Graduation",
     details: "wooohoo!!!",
@@ -16,29 +16,29 @@ const eventData = [
   {
     id: 2,
     name: "Holidays",
-    details: "wooohoo!!!",
+    details: "Break time!",
     date: "2024-12-24",
     background: "#f9f970",
   },
   {
     id: 3,
-    name: "HolidayYYYY",
-    details: "Get me outta here!",
-    date: "2024-12-31",
-    background: "#7AD3F0",
-  },
-  {
-    id: 4,
     name: "Birthday",
     details: "My birthday party",
     date: "2025-01-08",
     background: "#F07AEC",
   },
   {
-    id: 5,
-    name: "Christmas",
+    id: 4,
+    name: "Next Christmas",
     details: "ho ho ho",
     date: "2025-12-25",
+    background: "#165b33",
+  },
+  {
+    id: 5,
+    name: "Thanksgiving",
+    details: "Turkey time",
+    date: "2024-11-28",
     background: "#EB9A0F",
   },
   {
