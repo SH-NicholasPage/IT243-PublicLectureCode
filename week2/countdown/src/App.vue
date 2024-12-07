f4ab1feb9dde6014c9a823e75f3e31af912dd943
Nicholas Page <npage@southhills.edu>
2024-12-07 15:09:36 -0500

    Initial week 2 code

---
+++ week2/countdown/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <Event/>
+</template>
+
+<script>
+import Event from "@/components/Event.vue";
+
+const eventData = [
+  {
+    id: 1,
+    name: "Graduation",
+    details: "wooohoo!!!",
+    date: "2025-06-02",
+    background: "#F34949",
+  },
+  {
+    id: 2,
+    name: "Holidays",
+    details: "wooohoo!!!",
+    date: "2024-12-24",
+    background: "#f9f970",
+  },
+  {
+    id: 3,
+    name: "HolidayYYYY",
+    details: "Get me outta here!",
+    date: "2024-12-31",
+    background: "#7AD3F0",
+  },
+  {
+    id: 4,
+    name: "Birthday",
+    details: "My birthday party",
+    date: "2025-01-08",
+    background: "#F07AEC",
+  },
+  {
+    id: 5,
+    name: "Christmas",
+    details: "ho ho ho",
+    date: "2025-12-25",
+    background: "#EB9A0F",
+  },
+  {
+    id: 6,
+    name: "Conference Talk",
+    details: "dont flop",
+    date: "2025-02-28",
+    background: "#68EE94",
+  },
+];
+
+export default {
+  components: {
+    Event
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ week2/countdown/src/components/Event.vue
@@ -0,0 +1,58 @@
+<template>
+  
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+article
+{
+  background: lightslategray;
+  display: flex;
+  align-items: center;
+  border-radius: 1rem;
+  margin: 1rem 0;
+  padding: 0.2rem 1rem;
+  color: whitesmoke;
+  font-weight: 300;
+}
+
+.data
+{
+  flex: 3;
+}
+
+.countdown
+{
+  flex: 1;
+  text-align: center;
+  font-size: 1.6rem;
+  border-left: 1px solid;
+}
+
+.name
+{
+  font-size: 1.8rem;
+}
+
+.details
+{
+  font-size: 1.6rem;
+}
+
+/* remove event button: */
+.remove_btn
+{
+  background: transparent;
+  border: none;
+  font-size: 14px;
+  cursor: pointer;
+}
+
+.remove_btn_wrapper
+{
+  text-align: right;
+}
+</style>
